e94a22c4a649aeafd245d7072b7775e9dd70307b
studiobuttermedia <bebosua05@yahoo.com.vn>
2024-01-07 12:44:24 +0700

    Changes for a Seperate Release for Global

--- app.py
+++ app.py
@@ -11,16 +11,9 @@ import os
 server_now = datetime.datetime.now()
 log_time = server_now.strftime("%Y-%m-%d %H:%M:%S")
 
-filename = "time.txt"
-if not os.path.isfile(filename):
-   open(filename, "w").close()
-   print(f"Cache File '{filename}' was created. Please do not delete this file when the application is running")
-   print(f"To interrupt this program, press CTRL + C (CMD + C)")
-   print(f'[{log_time}] Running Program')
-else:
-   print(f"Cache file '{filename}' exists. Reusing file. Please do not delete this file when the application is running")
-   print(f"To interrupt this program, press CTRL + C (CMD + C)")
-   print(f'[{log_time}] Running Program')
+
+print(f"To interrupt this program, press CTRL + C (CMD + C)")
+print(f'[{log_time}] Running Program')
 
 load_dotenv()
 
@@ -39,37 +32,14 @@ while True:
    la_tz = pytz.timezone('America/Los_Angeles')
    la_time = datetime.datetime.now(la_tz)
 
-   cn_tz = pytz.timezone('Asia/Shanghai')
-   cn_time = datetime.datetime.now(cn_tz)
    if la_time.hour == 00 and la_time.minute == 00:
       la_tomorrow = la_time + datetime.timedelta(days=1)
-      print(f'[{log_time}] Setting Server Time (Sky - Global Server)')
+      print(f'[{log_time}] Setting Server Time')
       la_unix = int(la_tomorrow.timestamp())
-      with open('time.txt', 'r') as c:
-         cn_cache = str(c.read())
       y = requests.patch(url + '/messages/' + message_id, data={
     'username': 'Sky Time',
     'avatar_url': 'avatar',
-    'content': 'Global: Server Reset <t:' + str(la_unix) + ':R>\nChina: Server Reset <t:' + 
-    cn_cache+ ':R>'})
-      c.close
-      with open('time.txt', 'w') as f:
-         f.write(str(la_unix))
-   elif cn_time.hour == 00 and cn_time.minute == 00:
-      cn_tomorrow = cn_time + datetime.timedelta(days=1)
-      print(f'[{log_time}] Setting Server Time (Sky - NetEase/BiliBili Server)')
-      cn_unix = int(cn_tomorrow.timestamp())
-      with open('time.txt', 'r') as l:
-         la_cache = str(l.read())
-         a = requests.patch(url + '/messages/' + message_id, data={
-    'username': 'Sky Time',
-    'avatar_url': 'avatar',
-    'content': 'Global: Server Reset <t:' + la_cache + ':R>\nChina: Server Reset <t:' + 
-    str(cn_unix) + ':R>'})
-         a.close
-         with open('time.txt', 'w') as z:
-            z.write(str(cn_unix))
-   else: 
+    'content': 'Global: Server Reset <t:' + str(la_unix) + ':R>'})
       pass
 
    # Delay execution for 5 minutes
