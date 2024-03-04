2ba6dc4cf051ebfbdd4e9963e8520756dd866c0b
studiobuttermedia <bebosua05@yahoo.com.vn>
2024-03-04 19:35:20 +0700

    Ignore caches, update requirements and create a test script to connect to MySQL DB

---
+++ app_db.py
@@ -0,0 +1,25 @@
+from db_config import DB_CONFIG
+import pymysql
+
+def create_connection():
+    try:
+        connection = pymysql.connect(**DB_CONFIG)
+        print("Connected successfully!")
+        return connection
+    except pymysql.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
+
+if __name__ == "__main__":
+    db_connection = create_connection()
+    # Now you can use the connection for queries, inserts, etc.
+
+# Example query execution
+try:
+    with db_connection.cursor() as cursor:
+        sql = "SELECT * FROM data"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    db_connection.close()
